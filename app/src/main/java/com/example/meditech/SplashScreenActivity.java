ab99088362bec0b04ee407cdaabf8d06b7f460f0
mushaddek09 <mdmushaddekalmubdi@gmail.com>
2023-03-20 19:25:07 +0600

    I implement the splash screen in a good way

---
+++ app/src/main/java/com/example/meditech/SplashScreenActivity.java
@@ -0,0 +1,18 @@
+package com.example.meditech;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        startActivity(new Intent(SplashScreenActivity.this, OnBoarding.class));
+        finish();
+    }
+}
